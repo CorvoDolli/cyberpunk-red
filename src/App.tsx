c06625c84e98fcbb2a8a614fd8e817a28e6ca5d7
CorvoDolli <CorvoDolli@gmail.com>
2024-06-30 20:14:23 -0400

    Basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import GenericComp, { MainBody } from "./components/StyledComps";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.scss";
 
 function App() {
-	const [count, setCount] = useState(0);
+	// const [count, setCount] = useState(0);
 
 	return (
 		<MainBody>
